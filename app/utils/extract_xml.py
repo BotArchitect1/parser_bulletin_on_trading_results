51f45cb97fcb47fe8e3948df31b3c48347f64a69
BotArchitect1 <pavliksha@yandex.ru>
2024-06-07 15:56:06 +0300

    add new

--- app/utils/extract_xml.py
+++ app/utils/extract_xml.py
@@ -31,11 +31,14 @@ def extract_report_data(file_path):
                     exchange_product_id = row[headers['код инструмента']]
                     exchange_product_name = row[headers['наименование инструмента']]
                     delivery_basis_name = row[headers['базис поставки']]
-                    volume = row[headers['объем договоров в единицах измерения']]
-                    total = row[headers['обьем договоров, руб.']]
-                    count = row[headers['количество договоров, шт.']]
+                    volume_value = row[headers['объем договоров в единицах измерения']]
+                    volume = float(volume_value) if volume_value != '-' and volume_value != '' else 0.0
+                    total_value = row[headers['обьем договоров, руб.']]
+                    total = float(total_value) if total_value != '-' and total_value != '' else 0.0
+                    count_value = row[headers['количество договоров, шт.']]
+                    count = int(count_value) if count_value != '' and count_value != '-' else 0
 
-                    if count > '0' and exchange_product_id not in ('Итого:', 'Итого по секции:'):
+                    if count > 0 and exchange_product_id not in ('Итого:', 'Итого по секции:'):
                         data.append({
                             'exchange_product_id': exchange_product_id,
                             'exchange_product_name': exchange_product_name,
